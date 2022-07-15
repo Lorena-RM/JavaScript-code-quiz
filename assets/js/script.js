240915af724bfdd4bffcd6772e41ce89978634a5
Lorena Morales <Lorenarm.999@gmail.com>
2022-07-15 17:16:35 -0600

    show question Function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 //declare quaryselectors
 
 //start page query selectors
-const timerEl = document.querySelector (".timer")
+const timerEl = document.querySelector(".timer")
 const title = document.querySelector(".title");
 const info = document.querySelector(".instructions");
 const startbtn = document.querySelector(".btn");
@@ -19,43 +19,96 @@ let Gamescore;
 let score = 0;
 let initials = "";
 let timeLeft = 50;
+let currentQuestion = 0;
 
 //let highscores = JSON.parse(localStorage.getItem("highscores")) || [0];
 
 //declare question array
-//
+const questions = [
+    {
+        question: "commonly used data types DO NOT include: ",
+        answers: [
+            { text: "alerts", correct: true },
+            { text: "booleans", correct: false },
+            { text: "numbers", correct: false },
+            { text: "strings", correct: false },
+
+        ]
+    },
+    {
+        question: "The condition in an if / else statement is enclosed within ______. ",
+        answers: [
+            { text: "quotes", correct: false },
+            { text: "curly brackets", correct: false },
+            { text: "parantheses", correct: true },
+            { text: "square brackets", correct: false },
+
+        ]
+    },
+    {
+        question: "Arrays in JavaScript can be used to store _____. ",
+        answers: [
+            { text: "numbers and strings", correct: false },
+            { text: "other Arrays", correct: false },
+            { text: "booleans", correct: false },
+            { text: "all of the above", correct: true },
+
+        ]
+    },
+
+]
 
 
 startbtn.addEventListener("click", startGame);
-    //highScoresbutton.addEventListener("click", displayscores);
-
-    //Functions to begin the Game
-    // WHEN I click the start button
-    function startGame(){
-        //hide start button and instructions
-        startbtn.classList.add("hidden");
-        title.classList.add("hidden");
-        info.classList.add("hidden");
-
-        //add timer after start game btn clicked
-        var timer = setInterval(() => {
-            timeLeft--;
-            timerEl.textContent = "timer: " + timeLeft + " seconds remaining";
-
-            if(timeLeft <= 0) {
-                //endGame();
-                clearInterval(timer);
-            }
-        }, 1000);
-        //shows first question
-        showQuestionAry();
-    };
-    // THEN a timer starts and I am presented with a question
-    function showQuestionAry() {
-        
-    }
 
 
+//Functions to begin the Game
+// WHEN I click the start button
+function startGame() {
+    //hide start button and instructions
+    startbtn.classList.add("hidden");
+    title.classList.add("hidden");
+    info.classList.add("hidden");
+    timerEl.textContent = "timer: " + timeLeft + " seconds remaining";
+    timerEl.classList.remove('hidden')
+
+    //add timer after start game btn clicked
+    var timer = setInterval(() => {
+        timeLeft--;
+        timerEl.textContent = "timer: " + timeLeft + " seconds remaining";
+
+        if (timeLeft <= 0) {
+            //endGame();
+            clearInterval(timer);
+        }
+    }, 1000);
+    //shows first question
+    showQuestion();
+};
+// THEN a timer starts and I am presented with a question
+function showQuestion() {
+    questionSec.classList.remove("hidden");
+    questionEl.textContent = questions[currentQuestion].question
+    questions[currentQuestion].answers.forEach(answer=>{
+        //create a button
+        const optionButton = document.createElement('button');
+        optionButton.textContent= answer.text;
+        optionButton.classList.add('btn')
+        options.append(optionButton)
+    })
+    // questions.forEach(answer => {
+    //     const button =document.createElement
+    //     button.innerHTML = answer.text;
+    //     button.classList.add("btn");
+    //     if(answer.correct) {
+    //         button.dataset.correct = answer.correct;
+    //     }
+    // });
+}
+
+
+
+    
 
 
 // WHEN I answer a question
